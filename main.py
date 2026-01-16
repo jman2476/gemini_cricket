174eafe6a1afe76de3b53e87706645b4bd16d254
Squirblechem <152367545+jman2476@users.noreply.github.com>
2026-01-16 14:04:11 -0500

    add verbose flag

--- main.py
+++ main.py
@@ -6,9 +6,17 @@ from google.genai import types
 
 # Argparse
 parser = argparse.ArgumentParser(description='Chatbot')
-parser.add_argument('user_prompt', type=str, help='User prompt: Tell the agent what you want it to do.')
+parser.add_argument(
+        'user_prompt', 
+        type=str, 
+        help='User prompt: Tell the agent what you want it to do.'
+        )
+parser.add_argument(
+        '--verbose',
+        action='store_true',
+        help='Enable verbose output'
+        )
 args = parser.parse_args()
-print("User prompt", args)
 # Load .env and api key
 load_dotenv() 
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -29,8 +37,10 @@ if response.usage_metadata == None:
             'No response recieved. Please check your internet, or that the model you are calling is active.'
             )
 else:
-    print(
-        'Prompt tokens:', response.usage_metadata.prompt_token_count, '\n',
-        'Response tokens:', response.usage_metadata.candidates_token_count, '\n'
-        )
+    if args.verbose:
+        print(
+            'User prompt:', args, 
+            '\nPrompt tokens:', response.usage_metadata.prompt_token_count,
+            '\nResponse tokens:', response.usage_metadata.candidates_token_count
+            )
 print('Gemini response:', response.text)
