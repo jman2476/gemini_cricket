aa640c72d5ce2d825c76f978aea0dd6535342593
Squirblechem <152367545+jman2476@users.noreply.github.com>
2026-01-16 13:27:18 -0500

    add argparse, user input

--- main.py
+++ main.py
@@ -1,6 +1,37 @@
-def main():
-    print("Hello from gemini-agent!")
+import os
+import argparse
+from dotenv import load_dotenv
+from google import genai, types
+
+# Argparse
+parser = argparse.ArgumentParser(description='Chatbot')
+parser.add_argument('user_prompt', type=str, help='User prompt: Tell the agent what you want it to do.')
+args = parser.parse_args()
+print("User args", args)
+# Load .env and api key
+load_dotenv() 
+api_key = os.environ.get("GEMINI_API_KEY")
+if api_key == None: raise RuntimeError('No API key found. Generate one via Google AI Studio, and add to .env file')
+else: print(f'API key: ...{api_key[-8:]}')
+
+# Create Gemini client
+client = genai.Client(api_key=api_key)
+prompt = args.user_prompt if args else "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
 
 
-if __name__ == "__main__":
-    main()
+# Call Gemini API
+print('User prompt:', prompt)
+response = client.models.generate_content(
+        model='gemini-2.5-flash', 
+        contents=prompt
+        )
+if response.usage_metadata == None: 
+    raise RuntimeError(
+            'No response recieved. Please check your internet, or that the model you are calling is active.'
+            )
+else:
+    print(
+        'Prompt tokens:', response.usage_metadata.prompt_token_count, '\n',
+        'Response tokens:', response.usage_metadata.candidates_token_count, '\n'
+        )
+print('Gemini response:', response.text)
