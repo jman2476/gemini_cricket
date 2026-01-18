c9949527576ecdbe71a0f85bec8694702fb19fbf
Squirblechem <152367545+jman2476@users.noreply.github.com>
2026-01-18 14:06:05 -0500

    backup incase cricket screws up

--- prompts.py
+++ prompts.py
@@ -9,4 +9,6 @@ When a user asks a question or makes a request, make a function call plan. You c
 - Write or overwrite files
 
 All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
+
+If you are unsure where a file is, look through the root directory until you find what you need.
 """
